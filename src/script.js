d19512feeb38183f8ad5e36d6df759c58eee5f1a
Pedro Ribeiro <84580101+furPedroRibeiro@users.noreply.github.com>
2023-06-15 23:55:43 -0300

    Version 1.3 header e main prontos - home page

--- src/script.js
+++ src/script.js
@@ -12,6 +12,17 @@ function showSlider(){
     slider[currentSlide].classList.add('on');
 }
 
+
+setInterval(function nextSlider(){
+    hideSlider();
+    if(currentSlide === slider.length -1){
+        currentSlide = 0
+    } else{
+        currentSlide ++
+    }
+    showSlider()
+}, 7000);
+
 function nextSlider(){
     hideSlider();
     if(currentSlide === slider.length -1){
@@ -34,3 +45,56 @@ function prevSlider(){
 
 btnNext.addEventListener('click', nextSlider);
 btnPrev.addEventListener('click', prevSlider);
+
+/* script do nav para tela grande(pc), mudanças de estilo e navegação em si */
+
+const itemOne = document.getElementById('item-one');
+const itemTwo = document.getElementById('item-two');
+const itemThree = document.getElementById('item-three');
+const itemFour = document.getElementById('item-four');
+
+itemOne.onclick = function itemOneAddStyle(){
+    itemOne.classList.add('menu-item-select');
+    itemTwo.classList.remove('menu-item-select');
+    itemThree.classList.remove('menu-item-select');
+    itemFour.classList.remove('menu-item-select');
+}
+itemTwo.onclick = function itemTwoAddStyle(){
+    itemTwo.classList.add('menu-item-select');
+    itemOne.classList.remove('menu-item-select');
+    itemThree.classList.remove('menu-item-select');
+    itemFour.classList.remove('menu-item-select');
+}
+itemThree.onclick = function itemThreeAddStyle(){
+    itemThree.classList.add('menu-item-select');
+    itemTwo.classList.remove('menu-item-select');
+    itemOne.classList.remove('menu-item-select');
+    itemFour.classList.remove('menu-item-select');
+}
+itemFour.onclick = function itemOneAddStyle(){
+    itemFour.classList.add('menu-item-select');
+    itemTwo.classList.remove('menu-item-select');
+    itemThree.classList.remove('menu-item-select');
+    itemOne.classList.remove('menu-item-select');
+}
+
+/* Mostrando o menu do mobile quando clicar no ícone de menu */
+
+const btnMenuMobile = document.getElementById('btn-menu-mobile');
+
+btnMenuMobile.onclick = function showMenu(){
+    document.getElementById("nav-mobile").style.display = "flex";
+    document.getElementById("content-pc").style.display = "none"
+    // document.getElementById("body").style.overflow = "hidden"
+}
+
+const closeMenuMobile = document.getElementById('close-menu-mobile');
+
+closeMenuMobile.onclick = function hiddenMenu(){
+    document.getElementById("nav-mobile").style.display = "none";
+    document.getElementById("content-pc").style.display = "block"
+    document.getElementById("body").style.overflow = "auto"
+}
+
+
+
